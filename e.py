d07e612123e5b947f7596c7ab519c59503792ce2
Ingo Wolf <me@ingowolf.au>
2025-04-01 23:23:30 +0000

    show progress

--- e.py
+++ e.py
@@ -6,6 +6,7 @@ getcontext().prec = int(input("Precision: "))
 result = Decimal(0)
 
 for i in range(0, getcontext().prec+2):
+    print(f"\rCalculating {i}/{getcontext().prec+2}      ", end="")
     result = result + Decimal(1/math.factorial(i))
 
 print(result)
